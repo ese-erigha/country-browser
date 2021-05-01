0f2f2d92787451fb8d72bdbdc87f072d35d2526e
Eseoghene Erigha <ese.erigha@gmail.com>
2021-05-02 00:37:34 +0100

    nits

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    presets: ['react-app'],
-    plugins: [['react-remove-properties', { properties: ['data-testid'] }]],
+  presets: ['react-app'],
+  plugins: [['react-remove-properties', { properties: ['data-testid'] }]],
 };
