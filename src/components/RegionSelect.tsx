44488e367138bb55b8de8b454b74069a3cfa28d6
Eseoghene Erigha <ese.erigha@gmail.com>
2021-05-06 00:55:23 +0100

    Update RegionSelect.tsx

--- src/components/RegionSelect.tsx
+++ src/components/RegionSelect.tsx
@@ -6,10 +6,12 @@ const RegionSelect = () => {
   console.log('Hello');
   return (
     <div className="region-selector">
-      <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      <DropdownButton id="dropdown-basic-button" title="Filter by Region">
+        <Dropdown.Item href="#/action-1">Africa</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">America</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Asia</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Europe</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Oceania</Dropdown.Item>
       </DropdownButton>
     </div>
   );
