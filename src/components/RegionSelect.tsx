a5404e9a97e4132711e9f4f665c1f5f55e570fff
Eseoghene Erigha <ese.erigha@gmail.com>
2021-05-21 05:14:42 +0100

    nits

--- src/components/RegionSelect.tsx
+++ src/components/RegionSelect.tsx
@@ -20,35 +20,35 @@ const RegionSelect = () => {
   const {
     state: { activeQuery },
   } = useContext(AppContext);
-  const [selectedRegionValue, setSelectedRegionValue] = useState('');
-  const [dropdownText, setDropdownText] = useState('');
+  const [selectedRegionKey, setSelectedRegionKey] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('');
   const { fetchData } = useFetchData(ActionTypes.FETCH_COUNTRIES_BY_REGION_QUERY);
 
   const handleSelect = (eventKey: string | null) => {
     const key = eventKey!;
     const newVariables = buildQueryVariables({ offset: 0, region: key });
-    setDropdownText(regionMap[key]);
-    setSelectedRegionValue(key);
+    setSelectedRegion(regionMap[key]);
+    setSelectedRegionKey(key);
     fetchData(newVariables);
   };
 
   useEffect(() => {
-    const regionValue = activeQuery?.value?.region;
-    if (regionValue) setSelectedRegionValue(regionMap[regionValue]);
-    setDropdownText(regionValue ?? 'Filter by Region');
+    const regionKey = activeQuery?.value?.region;
+    let regionText = 'Filter by Region';
+    if (regionKey) {
+      setSelectedRegionKey(regionKey);
+      regionText = regionMap[regionKey];
+    }
+    setSelectedRegion(regionText);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="region-selector">
-      <DropdownButton onSelect={handleSelect} id="dropdown-basic-button" title={dropdownText}>
-        {regionMapEntries.map((region) => (
-          <Dropdown.Item
-            active={selectedRegionValue === region[0]}
-            key={region[0]}
-            eventKey={region[0]}
-          >
-            {region[1]}
+      <DropdownButton onSelect={handleSelect} id="dropdown-basic-button" title={selectedRegion}>
+        {regionMapEntries.map(([key, value]) => (
+          <Dropdown.Item active={selectedRegionKey === key} key={key} eventKey={key}>
+            {value}
           </Dropdown.Item>
         ))}
       </DropdownButton>
